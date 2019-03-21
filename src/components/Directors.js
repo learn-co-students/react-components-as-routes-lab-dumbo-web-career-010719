ae417eae2e3d583675847b9f1d7c72b4e079b2eb
NorbertLam <noobertlm@gmail.com>
2019-03-21 10:39:29 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorArr = directors.map(director => {
+    return (<div>
+      <h1>{director.name}</h1>
+      <h3>Movies:</h3>
+      <ul>
+        {director.movies.map(movie => <li>{movie}</li>)}
+      </ul>
+    </div>)
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorArr}
     </div>
   );
 }
