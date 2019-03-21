b34588592bac1e92dd4df4f90080fe5286dd73e6
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-03-20 20:44:48 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+ const moviesArr = movies.map(movie => {
+   return <div>
+            <p>Title: {movie.title}</p>
+            <p>Time: {movie.time}</p>
+            <ul>
+              {movie.genres.map(genre => {
+               return <li>{genre}</li>
+              })}
+            </ul>
+         </div>
+  })
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+        {moviesArr}
+        
     </div>
   );
 };
