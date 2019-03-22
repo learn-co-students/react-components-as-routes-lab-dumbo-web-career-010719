0fafcf63b67ec9aec726496a73c77333fa559d49
Elizabeth Le <elizabethlemk@yahoo.com>
2019-03-22 09:25:35 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movie =>
+          <div>
+            <h1>{movie.title}</h1>
+            <h5>{movie.time} mins</h5>
+            <ul>Genres: {movie.genres.map(genre => <li>{genre}</li> )}</ul>
+            <h5>Metascore: {movie.metascore}</h5>
+          </div>
+        )}
     </div>
   );
 };
